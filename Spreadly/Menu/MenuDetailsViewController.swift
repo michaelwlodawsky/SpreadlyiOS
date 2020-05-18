8fd508065fd3b3d6d8c49117b5c3669699032b72
Michael Wlodawsky <michaelwlodawsky@gmail.com>
2020-05-17 20:03:14 -0500

    mvp alpha complete

--- Spreadly/Menu/MenuDetailsViewController.swift
+++ Spreadly/Menu/MenuDetailsViewController.swift
@@ -14,6 +14,11 @@ class MenuDetailsViewController: UIViewController {
     @IBOutlet weak var itemPrice: UILabel!
     @IBOutlet weak var itemTitle: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var veggieStackView: UIStackView!
+    @IBOutlet weak var pescatarianStackView: UIStackView!
+    @IBOutlet weak var gfStackView: UIStackView!
+    @IBOutlet weak var veggieImage: UIImageView!
+    @IBOutlet weak var veggieLabel: UILabel!
     
     var item: MenuItem? = nil
 
@@ -22,7 +27,21 @@ class MenuDetailsViewController: UIViewController {
         itemDescription.text = item?.description
         itemPrice.text = String(format: "$%0.2f", item!.price)
         itemTitle.text = item?.name
-        imageView.image = item?.image
+        imageView.image = item?.image ?? UIImage(named: "Default.png")
+        
+        if item?.vegan ?? false {
+            veggieImage.image = UIImage(named: "Vegan.png")
+            veggieLabel.text = "Vegan"
+        }
+        if item?.vegan ?? false || item?.vegetarian ?? false {
+            veggieStackView.isHidden = false
+        }
+        if item?.pescatarian ?? false {
+            pescatarianStackView.isHidden = false
+        }
+        if item?.gf ?? false {
+            gfStackView.isHidden = false
+        }
     }
 
     @IBAction func onExit(_ sender: Any) {
